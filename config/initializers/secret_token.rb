2166fb0f9101344239bf33f87e8ad271ebde8a7d
jmacpherson <jd.macpherson@gmail.com>
2014-03-12 17:06:06 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VajraLodge::Application.config.secret_key_base = '90217c5c6931e39a6d511372f8be0b92687947da0904c5ec500d9802759e51f695118b2164707dd76176fc59fb6cb14fd54935059cb0f7be1549a108167c9308'
