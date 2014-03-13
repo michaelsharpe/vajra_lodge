f421c418b0974a1147a2cbf771f9d4dfa1c6910f
jmacpherson <jd.macpherson@gmail.com>
2014-03-13 18:52:30 -0400

    added bio to User

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ r = Random.new
 roles = %w[admin treasurer secretary]
 
 1.upto(5) do |i|
-  User.create!(username: "user#{i}", email: "test@test.com", password:"test888",password_confirmation:"test888",roles: "#{roles[i-1]}",degree: r.rand(1..33))
+  User.create!(username: "user#{i}", email: "test@test.com", password:"test888",password_confirmation:"test888",roles: "#{roles[i-1]}",degree: r.rand(1..33), bio: Faker::Lorem.paragraphs(r.rand(1..3)).join("\n\n"))
 end
 
 100.times do |i|
-  Post.create!(user_id: r.rand(1..5), degree: r.rand(1..33), content: Faker::Lorem.paragraphs(r.rand(1..5)).join("\n"))
+  Post.create!(user_id: r.rand(1..5), degree: r.rand(1..33), content: Faker::Lorem.paragraphs(r.rand(1..5)).join("\n\n"))
 end
