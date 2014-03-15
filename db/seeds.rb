c06e6272732b13a9e98d44fc18e932170deb105e
jmacpherson <jd.macpherson@gmail.com>
2014-03-15 14:50:34 -0400

    adjusted case of roles in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 r = Random.new
-roles = %w[admin treasurer secretary]
+roles = %w[Admin Treasurer Secretary]
 
 1.upto(5) do |i|
   User.create!(username: "user#{i}", email: "test#{i}@test.com", password:"test888",password_confirmation:"test888",roles: "#{roles[i-1]}",degree: r.rand(1..33), bio: Faker::Lorem.paragraphs(r.rand(1..3)).join("\n\n"), disabled: false)
