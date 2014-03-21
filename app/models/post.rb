0a4815a42c5a5a9048b68a738665b20cc1249d43
jmacpherson <jd.macpherson@gmail.com>
2014-03-20 21:40:36 -0400

    limited degree options for posting to those <= current_user.degree

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,8 @@ class Post < ActiveRecord::Base
   validates_presence_of :content
 
   DEGREES = %w(1 2 3 18 30 31 32 33)
+
+  def self.degrees_selection(current_user)
+    degrees = DEGREES.keep_if { |d| d.to_i <= current_user.degree }
+  end
 end
