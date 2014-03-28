72b00d4889ecf45f823fe34c06f0e4bc06855839
jmacpherson <jd.macpherson@gmail.com>
2014-03-28 17:27:42 -0400

    create and update milestones

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -15,21 +15,42 @@ class MilestonesController < ApplicationController
   end
 
   def new
+    @milestone = @user.milestones.new
   end
 
   def create
+    @milestone = @user.milestones.build(milestone_params)
+    @milestone.user_id = @user.id
+    if @milestone.save
+      redirect_to("/users/#{@user.id}/milestones", notice: "New milestone created for #{@user.username}")
+    else
+      flash.now[:alert] = "Unable to save milestone"
+      render "new"
+    end
   end
 
   def edit
+    @milestone = Milestone.find(params[:id])
   end
 
   def update
+    @milestone = Milestone.find(params[:id])
+    if @milestone.update_attributes(milestone_params)
+      redirect_to("/users/#{@user.id}/milestones", notice: "Updated.")
+    else
+      flash.now[:alert] = "Edit failed"
+      render "edit"
+    end
   end
 
   def destroy
   end
 
   private
+  def milestone_params
+    params.require(:milestone).permit(:category, :event, :date, :location, :description)
+  end
+
   def load_user
     @user = User.find(params[:user_id])
   end
